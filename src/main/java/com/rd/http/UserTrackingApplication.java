a29d541729305d5092da6a611408c13065b08d04
marcus.vinicius <marcus.vinicius@pixeon.com>
2017-10-26 15:32:40 -0200

    Adding REST Sercice Resgistration

---
+++ src/main/java/com/rd/http/UserTrackingApplication.java
@@ -0,0 +1,20 @@
+package com.rd.http;
+
+import com.rd.http.UserTrackingService;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserTrackingApplication extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+
+    public UserTrackingApplication() {
+        singletons.add(new UserTrackingService());
+    }
+
+    @Override
+    public final Set<Object> getSingletons() {
+        return singletons;
+    }
+}
